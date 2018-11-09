46c3cd237544c590c3e54d70e2589be12a18d442
Chris Andersson <candersson@klarsolutions.com>
2018-11-09 15:17:02 -0500

    Initial Commit

---
+++ click_counter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  click_counter
+//
+//  Created by Chris Andersson on 2018-11-09.
+//  Copyright © 2018 klarsolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
