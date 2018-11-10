d0e1b40df84ed05e6778f32dc08559d09ded3801
Chris Andersson <candersson@klarsolutions.com>
2018-11-10 08:39:46 -0500

    initial commit of click counter

--- click_counter/ViewController.swift
+++ click_counter/ViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
 
+    var count = 0
+    @IBOutlet var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // label
+       
     }
-
-
+    @IBAction func incrementCount() {
+        self.count += 1
+        self.label.text = "\(self.count)"
+        }
+    
 }
 
